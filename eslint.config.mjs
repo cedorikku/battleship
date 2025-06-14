e57a7776dad747b969f75a44e6bd36ddb5c73725
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-06-14 15:13:11 +0800

    Add eslint jest plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginJest from 'eslint-plugin-jest';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -9,7 +10,21 @@ export default [
     eslintConfigPrettier,
     {
         rules: {
-            "no-unused-vars": "warn",
+            'no-unused-vars': 'warn',
+        },
+    },
+    {
+        files: ['**/*.spec.js', '**/*.test.js'], // test files
+        plugins: { jest: pluginJest },
+        languageOptions: {
+            globals: pluginJest.environments.globals.globals,
+        },
+        rules: {
+            'jest/no-disabled-tests': 'warn',
+            'jest/no-focused-tests': 'error',
+            'jest/no-identical-title': 'error',
+            'jest/prefer-to-have-length': 'warn',
+            'jest/valid-expect': 'error',
         },
     },
 ];
