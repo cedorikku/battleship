4d664d09de2ee40224a1b05a6dae30c9fae3a66f
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-06-17 15:18:17 +0800

    Init test for ship class

---
+++ tests/ship.test.js
@@ -0,0 +1,31 @@
+import { describe, expect, test } from '@jest/globals';
+import Ship from '../src/modules/ship';
+
+describe('Ship is hit', () => {
+    const length = 4;
+
+    test('Ship tracks its own hits', () => {
+        const ship = new Ship(length);
+        ship.hit();
+        expect(ship.hits).toBe(1);
+    });
+
+    test('Ship with length gets sunk', () => {
+        const ship = new Ship(length);
+        for (let i = 0; i < ship.length; i++) {
+            ship.hit();
+        }
+        expect(ship.isSunk()).toBe(true);
+    });
+
+
+    test('Ship does not take hit if sunk', () => {
+        const ship = new Ship(length);
+
+        for (let i = 0; i < ship.length + 1; i++) {
+            ship.hit();
+        }
+       
+        expect(ship.hits).toBe(ship.length);
+    });
+});
