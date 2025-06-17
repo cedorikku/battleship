1f335c731be5b8b9dfa64841fc8d905281a35c83
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-06-17 13:46:49 +0800

    Add isBot property to player

--- src/modules/player.js
+++ src/modules/player.js
@@ -2,9 +2,10 @@ import Gameboard from './gameboard.js';
 
 // Should differntiate between 'real' and 'computer' players
 class Player {
-    constructor(name) {
+    constructor(name, isBot) {
         this.board = new Gameboard();
         this.name = name;
+        this.isBot = isBot; 
     }
 }
 
