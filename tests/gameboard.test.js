c1b371848eec652514d6a553c851bd7438a5eccf
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-06-18 17:24:47 +0800

    update test to isolate instances pt2

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -2,20 +2,22 @@ import { describe, expect, test } from '@jest/globals';
 import Gameboard from '../src/modules/gameboard';
 
 describe('Ship placements', () => {
-    const gb = new Gameboard();
-
     // places a ship in 0, 0
     test('Accepts free space', () => {
+        const gb = new Gameboard();
         expect(gb.placeShip(0, 0)).toBe(0);
     });
 
     // places another ship in 0, 0
     test('Rejects occupied space', () => {
+        const gb = new Gameboard();
+        gb.placeShip(0, 0);
         expect(gb.placeShip(0, 0)).toBe(1);
     });
 
     // out of bounds
     test('Rejects space that is obviously out of bounds', () => {
+        const gb = new Gameboard();
         expect(gb.placeShip(-1, 11)).toBe(-1);
     });
 
