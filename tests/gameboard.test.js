349de95f689f7f4bf6f9b14737990bbe7df36dd0
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-06-18 23:57:32 +0800

    wip: update test to handle orientations and lengths

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -3,46 +3,48 @@ import Gameboard from '../src/modules/gameboard';
 
 describe('Ship placements', () => {
     // places a ship in 0, 0
-    test('Accepts free space', () => {
+    test('Accepts free spaces', () => {
         const gb = new Gameboard();
-        expect(gb.placeShip(0, 0)).toBe(0);
+        expect(gb.placeShip(0, 0, 2, true)).toBe(0); // vertical ship with 2 length
+        expect(gb.placeShip(0, 1, 3, true)).toBe(0); // vertical ship with 3 length
+        expect(gb.placeShip(0, 2, 4, true)).toBe(0); // vertical ship with 4 length
     });
 
-    // places another ship in 0, 0
-    test('Rejects occupied space', () => {
+    // occupied or out of bounds
+    test('Rejects unavailable and invalid root placement for the ship', () => {
         const gb = new Gameboard();
-        gb.placeShip(0, 0);
-        expect(gb.placeShip(0, 0)).toBe(1);
+        gb.placeShip(0, 0, 2, true); // vertical ship with 2 length
+
+        expect(gb.placeShip(0, 0, 2)).toBe(-1);
+        expect(gb.placeShip(-1, 11, 2)).toBe(-1);
     });
 
-    // out of bounds
-    test('Rejects space that is obviously out of bounds', () => {
-        const gb = new Gameboard();
-        expect(gb.placeShip(-1, 11)).toBe(-1);
-    });
-
-    // TODO ship isn't the right size
+    // TODO: Add test case to reject invalid body-tail placement
 });
 
 describe('Receive ship hit', () => {
     test('Registers attack as hit', () => {
         const gb = new Gameboard();
-        gb.placeShip(0, 0);
+        gb.placeShip(0, 0, 2, true); // vertical ship with 2 length
 
         expect(gb.receiveAttack(0, 0)).toBe(0);
+        expect(gb.receiveAttack(1, 0)).toBe(0);
     });
 
     test('Registers attack as miss', () => {
         const gb = new Gameboard();
-        gb.placeShip(0, 0);
 
+        gb.placeShip(0, 0, 2, true); // vertical ship with 2 length
+
+        expect(gb.receiveAttack(0, 1)).toBe(1);
         expect(gb.receiveAttack(1, 1)).toBe(1);
     });
 
     test('Recognize space as already attacked', () => {
         const gb = new Gameboard();
-        gb.placeShip(0, 0);
-        gb.placeShip(1, 1);
+        gb.placeShip(0, 0, 2, true); // vertical ship with 2 length
+        gb.placeShip(1, 1, 3, true); // vertical ship with 3 length
+
         gb.receiveAttack(0, 0);
         gb.receiveAttack(1, 1);
 
