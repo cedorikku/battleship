1c9dc694bef605b3ff29fccc4ad06456b8a133ea
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-06-19 12:36:26 +0800

    Init entry file

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './assets/styles/index.css';
+import GameController from './modules/gameController.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // render initial UI
+    const game = new GameController();
+
+    // render empty grid
+    game.loadMenu();
+    
+    // TEMP: audomatically starts the game
+    game.startGame();
+});
+
