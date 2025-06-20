b4d3eef9475a1d920bbccae18d984a715ac7dd98
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-06-20 13:42:10 +0800

    Add global ship variants in config

--- src/modules/config.js
+++ src/modules/config.js
@@ -1,5 +1,12 @@
 class Config {
     static boardSize = 10;
+    static variants = Object.freeze({
+        carrier: 5,
+        battleship: 4,
+        cruiser: 3,
+        submarine: 2,
+        destroyer: 2,
+    });
 }
 
 export default Config;
