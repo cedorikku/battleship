bd56e0c589b094222ff63d2ac3f0be5fba58752c
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-07-12 17:30:39 +0800

    chore: add docs to config for the toolings

--- src/modules/battleshipConfig.js
+++ src/modules/battleshipConfig.js
@@ -1,3 +1,27 @@
+/**
+ * @typedef {Object} ShipVariants
+ * @property {number} CARRIER - Length of carrier ship
+ * @property {number} BATTLESHIP - Length of battleship
+ * @property {number} CRUISER - Length of cruiser
+ * @property {number} SUBMARINE - Length of submarine
+ * @property {number} DESTROYER - Length of destroyer
+ */
+
+/**
+ * @typedef {Object} Orientation
+ * @property {boolean} VERTICAL - Vertical orientation (true)
+ * @property {boolean} HORIZONTAL - Horizontal orientation (false)
+ */
+
+/**
+ * Battleship configuration constants
+ * @typedef {Object} BattleshipConfig
+ * @property {number} BOARD_SIZE - The size of the game board (e.g., 10 for a 10x10 grid)
+ * @property {ShipVariants} SHIP_VARIANTS - Ship types and their lengths
+ * @property {Orientation} ORIENTATION - Ship orientation flags
+ */
+
+/** @type {BattleshipConfig} */
 const battleshipConfig = {
     BOARD_SIZE: 10,
     SHIP_VARIANTS: Object.freeze({
